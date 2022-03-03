ae6443950cbb7eea255d1b248b9fcc5394459ed6
AlfredoGutierrez4ce <alfredo.gutierrez@4ce.com.mx>
2022-03-03 19:02:49 +0000

    [ADD] Basic Module

---
+++ space_mision/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Space mission',
+    
+    'sumary': """Logistic app to manage Space mission to moon""",
+    
+    'description': """
+        Space mission to organize logistics of mission to the moon
+        - Spaceship
+        - Space crew
+    """,
+    
+    'author': 'odoo'
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Logistics',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ],
+    
+    'demo': [
+        
+        
+    ],
+    
+}
